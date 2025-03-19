60b90f7f983872ac668c21b12269d707ca3020b8
TarikCampestrini <tarikrc@hotmail.com>
2025-03-19 12:08:03 -0300

    Correção de rotas

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -17,7 +17,7 @@ const Index = () => {
     setError("");
 
     try {
-      const response = await fetch("api/auth/login", { 
+      const response = await fetch("/api/auth/login", { 
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://app-condominio-backend.vercel.app/", 
+        target: "http://localhost:3000/", 
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
