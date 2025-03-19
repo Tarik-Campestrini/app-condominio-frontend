112b5825b384f709b32b6cfb2d6c102f5f647145
Tarik Campestrini <tarikrc@hotmail.com>
2025-03-18 23:40:56 -0300

    Config Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export const content = [
-  './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/pages/**/*.{js,ts,jsx,tsx,mdx,html}',
   './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   './src/app/**/*.{js,ts,jsx,tsx,mdx}',
 ];
